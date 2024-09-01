0a51e78aa27397db66d96ef01b892b0d7710bd1d
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-09-01 13:42:22 +0530

    Update Code

--- ingest.sh
+++ ingest.sh
@@ -1,2 +1,1 @@
-wget -O - https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/city_market_tracker.tsv000.gz
-| aws s3 cp - s3://redfin-data-yml/store-raw-data-yml/city_market_tracker.tsv000.gz
+wget -O - https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/city_market_tracker.tsv000.gz | aws s3 cp - s3://redfin-data-yml/store-raw-data-yml/city_market_tracker.tsv000.gz
