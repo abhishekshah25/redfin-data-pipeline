4309dfcd11908d8484318208cf718570dc3014ed
Abhishek Shah <147745895+abhishekshah25@users.noreply.github.com>
2024-10-04 12:28:34 +0530

    Update Code

--- redfin_analytics.py
+++ redfin_analytics.py
@@ -12,7 +12,7 @@ default_args = {
     'owner': 'Smudgy',
     'depends_on_past': False,
     'start_date': datetime(2024, 6, 26),
-    'email': ['smudgy@gmail.com'],
+    'email': ['smudger49@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 2,
